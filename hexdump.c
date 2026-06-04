492e022f699667cc4811a84242ab16929d0fe543
Selinam <selinam.mortey@yale.edu>
2026-06-04 11:27:26 -0700

    Adding offset column to indicate byte position per row.

--- hexdump.c
+++ hexdump.c
@@ -15,12 +15,17 @@ int main (int argc, char *argv[]) {
 
     unsigned char buffer[16];
     size_t bytes_read;
+    size_t offset = 0;
 
     while ((bytes_read = fread(buffer, 1, 16, fp)) > 0) {
+
+        printf("%08zx  ", offset);
         for (size_t i = 0; i < bytes_read; i++) {
             printf("%02x ", buffer[i]);
         }
         printf("\n");
+
+        offset += bytes_read;
     }
 
 
